7b66069454347c7cbe23903eb48d6309c7fd5317
young6665 <hiyoung0520@gmail.com>
2026-09-02 15:07:17 +0800

    style: format language map

--- standalone/source/main.cpp
+++ standalone/source/main.cpp
@@ -8,10 +8,8 @@
 
 auto main(int argc, char** argv) -> int {
   const std::unordered_map<std::string, greeter::LanguageCode> languages{
-      {"en", greeter::LanguageCode::EN},
-      {"de", greeter::LanguageCode::DE},
-      {"es", greeter::LanguageCode::ES},
-      {"fr", greeter::LanguageCode::FR},
+      {"en", greeter::LanguageCode::EN}, {"de", greeter::LanguageCode::DE},
+      {"es", greeter::LanguageCode::ES}, {"fr", greeter::LanguageCode::FR},
       {"zh", greeter::LanguageCode::ZH},
   };
 
