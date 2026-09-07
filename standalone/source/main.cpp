813d20f92239fadecb6745951ea638df56a15687
young6665 <hiyoung0520@gmail.com>
2026-09-07 14:57:41 +0800

    feat: add interactive MiniChat message

--- standalone/source/main.cpp
+++ standalone/source/main.cpp
@@ -1,52 +1,18 @@
-#include <greeter/greeter.h>
-#include <greeter/version.h>
-
-#include <cxxopts.hpp>
 #include <iostream>
 #include <string>
-#include <unordered_map>
 
-auto main(int argc, char** argv) -> int {
-  const std::unordered_map<std::string, greeter::LanguageCode> languages{
-      {"en", greeter::LanguageCode::EN}, {"de", greeter::LanguageCode::DE},
-      {"es", greeter::LanguageCode::ES}, {"fr", greeter::LanguageCode::FR},
-      {"zh", greeter::LanguageCode::ZH},
-  };
+struct Message {
+    std::string sender;
+    std::string content;
+};
 
-  cxxopts::Options options(*argv, "A program to welcome the world!");
-
-  std::string language;
-  std::string name;
-
-  // clang-format off
-  options.add_options()
-    ("h,help", "Show help")
-    ("v,version", "Print the current version number")
-    ("n,name", "Name to greet", cxxopts::value(name)->default_value("World"))
-    ("l,lang", "Language code to use", cxxopts::value(language)->default_value("en"))
-  ;
-  // clang-format on
-
-  auto result = options.parse(argc, argv);
-
-  if (result["help"].as<bool>()) {
-    std::cout << options.help() << std::endl;
+int main() {
+    Message msg;
+    std::cout << "sender: ";
+    std::getline(std::cin, msg.sender);
+    std::cout << "content: ";
+    std::getline(std::cin, msg.content);
+    std::cout << msg.sender << ": " << msg.content << std::endl;
+    system("pause");
     return 0;
-  }
-
-  if (result["version"].as<bool>()) {
-    std::cout << "Greeter, version " << GREETER_VERSION << std::endl;
-    return 0;
-  }
-
-  auto langIt = languages.find(language);
-  if (langIt == languages.end()) {
-    std::cerr << "unknown language code: " << language << std::endl;
-    return 1;
-  }
-
-  greeter::Greeter greeter(name);
-  std::cout << greeter.greet(langIt->second) << std::endl;
-
-  return 0;
 }
